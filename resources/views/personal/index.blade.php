147cc6e1cd19134487d72f0a703fe46bddcf95b8
Максим Пивоваров <pimax1978@icloud.com>
2020-01-08 05:47:46 +0600

    Morning2

--- resources/views/personal/index.blade.php
+++ resources/views/personal/index.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="row">
     <div class="col-xs-12 col-md-3 col-lg-2">
-        <ul>
-            <li>
+        <ul class="list-group mb-3">
+            <li class="list-group-item d-flex justify-content-between lh-condensed">
                 <router-link :to="{name: 'auctionIndex'}">Auctions</router-link>
             </li>
-            <li>
+            <li class="list-group-item d-flex justify-content-between lh-condensed">
                 <router-link :to="{name: 'contragentIndex'}">Contragents</router-link>
             </li>
         </ul>
