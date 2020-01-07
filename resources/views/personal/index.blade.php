a571f29e2e1a35648f3b98679b7485abbfa5b51e
Максим Пивоваров <pimax1978@icloud.com>
2020-01-08 05:41:37 +0600

    Morning2

--- resources/views/personal/index.blade.php
+++ resources/views/personal/index.blade.php
@@ -2,13 +2,18 @@
 
 @section('content')
 <div class="row">
-    <div class="col-6">
-        <router-link :to="{name: 'auctionIndex'}" class="btn btn-primary">Auctions</router-link>
+    <div class="col-xs-12 col-md-3 col-lg-2">
+        <ul>
+            <li>
+                <router-link :to="{name: 'auctionIndex'}">Auctions</router-link>
+            </li>
+            <li>
+                <router-link :to="{name: 'contragentIndex'}">Contragents</router-link>
+            </li>
+        </ul>
     </div>
-    <div class="col-6">
-        <router-link :to="{name: 'contragentIndex'}" class="btn btn-primary">Contragents</router-link>
+    <div class="col-xs-12 col-md-9 col-lg-10">
+        <router-view></router-view>
     </div>
 </div>
-<hr>
-<router-view></router-view>
 @endsection
