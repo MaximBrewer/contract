67e474863d99ca05744e873716981e73035c6513
Максим Пивоваров <pimax1978@icloud.com>
2020-01-20 01:15:16 +0600

    ol

--- app/Http/Controllers/Api/V1/AuctionsController.php
+++ app/Http/Controllers/Api/V1/AuctionsController.php
@@ -293,7 +293,7 @@ class AuctionsController extends Controller
                 'volume' => (int) $result->volume + (int) $r->post('volume')
             ]);
         } else {
-            Result::add([
+            Result::create([
                 'auction_id' => $r->post('auction'),
                 'contragent_id' => $r->post('bidder'),
                 'price' => (float) $r->post('price'),
