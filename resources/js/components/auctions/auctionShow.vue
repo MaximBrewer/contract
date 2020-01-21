9b8f23c6f96f2e530eee08e725d16ab516c658ae
Максим Пивоваров <pimax1978@icloud.com>
2020-01-21 20:07:31 +0600

    Y

--- resources/js/components/auctions/auctionShow.vue
+++ resources/js/components/auctions/auctionShow.vue
@@ -645,7 +645,7 @@ export default {
       Echo.channel("cross_contractru_database_message-pushed").listen(
         "MessagePushed",
         function(e) {
-          console.log(e)
+          console.log(e.auction)
           if (app.auction.id == e.auction.id) app.auction == e.auction;
         }
       );
