93361633c681dfe8c50b188583c6b2059d01d57c
Максим Пивоваров <pimax1978@icloud.com>
2020-02-20 11:27:27 +0600

    :

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -128,8 +128,8 @@
                                 <router-link :to="{name: 'company'}" class="dropdown-item">{{ __('Settings') }}
                                 </router-link>
 
-                                <a class="dropdown-item" href="javascript.void(0)"
-                                    onclick="event.preventDefault();document.getElementById('logout-form')submit();">
+                                <a class="dropdown-item" href="javascript:void(0)"
+                                    onclick="document.getElementById('logout-form')submit();">
                                     {{ __('Logout') }}
                                 </a>
 
