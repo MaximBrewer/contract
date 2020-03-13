20d4998a4c781cb331c75c75e1d90000a4f3f248
Maxim <maxim@MacBook-Pro-Maxim.local>
2020-03-13 14:39:00 +0600

    M

--- app/Http/Controllers/Api/V1/MailController.php
+++ app/Http/Controllers/Api/V1/MailController.php
@@ -13,23 +13,27 @@ use Illuminate\Support\Facades\DB;
 class MailController extends Controller
 {
     //
-    public function send(Request $r){
-        if((int)$r->post('auction') && (string)$r->post('message')){
-            $auction = Auction::findOrFail((int)$r->post('auction'));
-            switch((int)$r->post('whom')){
+    public function send(Request $r)
+    {
+        if ((int) $r->post('auction') && (string) $r->post('message')) {
+            $auction = Auction::findOrFail((int) $r->post('auction'));
+
+            switch ((int) $r->post('whom')) {
                 case 0:
-                    $users = DB::table('users')->whereRaw('id in (select user_id from user_contragent where contragent_id in (select id from `contragent_auction` WHERE `auction_id`=?))', [$auction->id])->get();
-                break;
+                    $users = DB::table('users')->whereRaw('id in (select user_id from user_contragent where contragent_id in (select contragent_id from `contragent_auction` WHERE `auction_id`=?))', [$auction->id])->get();
+                    break;
                 case 1:
-                    $users = DB::table('users')->whereRaw('id in (select user_id from user_contragent where contragent_id in (select id from `contragent_auction` WHERE `observer`=1 and `auction_id`=?))', [$auction->id])->get();
-                break;
+                    $users = DB::table('users')->whereRaw('id in (select user_id from user_contragent where contragent_id in (select contragent_id from `contragent_auction` WHERE `observer`=1 and `auction_id`=?))', [$auction->id])->get();
+                    break;
                 case 2:
-                    $users = DB::table('users')->whereRaw('id in (select user_id from user_contragent where contragent_id in (select id from `contragent_auction` WHERE `observer`=1 and `can_bet`=1 and `auction_id`=?))', [$auction->id])->get();
-                break;
+                    $users = DB::table('users')->whereRaw('id in (select user_id from user_contragent where contragent_id in (select contragent_id from `contragent_auction` WHERE `observer`=1 and `can_bet`=1 and `auction_id`=?))', [$auction->id])->get();
+                    break;
             }
-            foreach($users as $user){
-                Mail::to($user)->send(new AuctionMail((string)$r->post('message')));
-            }
+
+            return [$users, $auction->id, (int) $r->post('whom')];
+            // foreach($users as $user){
+            //     Mail::to($user)->send(new AuctionMail((string)$r->post('message')));
+            // }
         } else {
             return response()->json([
                 'message' => __('It`s not yours!'),
