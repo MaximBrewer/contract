74873321090ac95be65e489aee7dd4b8d3bbc2b8
Maxim <maxim@MacBook-Pro-Maxim.local>
2020-04-03 12:30:02 +0600

    L

--- app/Mail/ResultMessage.php
+++ app/Mail/ResultMessage.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use App\Settlement;
 
 class ResultMessage extends Mailable
 {
@@ -30,10 +31,14 @@ class ResultMessage extends Mailable
      */
     public function build()
     {
+        $uids = Settlement::where('bet_id', $this->result->id)->get();
+        if(count($uids)) $uid = $uids[0];
+        else $uid = 0;
         return $this->subject('Не согласен с расчетами!')
             ->view('mail.result_message')->with([
                 'result' => $this->result,
-                'link' => 'http://project.cross-contract.ru/admin/bets/' . $this->result->id
+                'link' => 'http://project.cross-contract.ru/admin/bets/' . $this->result->id,
+                'ulink' => 'http://project.cross-contract.ru/admin/settlements/7/edit' . $uid
             ]);;
     }
 }
