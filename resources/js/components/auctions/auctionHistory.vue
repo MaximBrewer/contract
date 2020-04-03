a37c8f4e553bb8f39f265a81ff2716831169e5b1
Maxim <maxim@MacBook-Pro-Maxim.local>
2020-04-03 21:32:49 +0600

    L

--- resources/js/components/auctions/auctionHistory.vue
+++ resources/js/components/auctions/auctionHistory.vue
@@ -12,7 +12,7 @@
                 <th class="text-center">#</th>
                 <th class="text-center">{{ __('Price') }}</th>
                 <th class="text-center">{{ __('Volume') }}</th>
-                <th class="text-center">{{ __('Contragent') }}</th>
+                <th class="text-center" v-if="auction.contragent.id == company.id">{{ __('Contragent') }}</th>
                 <th class="text-center">{{ __('Time') }}</th>
               </tr>
             </thead>
@@ -21,7 +21,7 @@
                 <td class="text-center">{{ unit.id }}</td>
                 <td class="text-center">{{ unit.price }}</td>
                 <td class="text-center">{{ unit.volume }}</td>
-                <td class="text-center">{{ unit.contragent.title }}</td>
+                <td class="text-center" v-if="auction.contragent.id == company.id">{{ unit.contragent.title }}</td>
                 <td class="text-center">{{ unit.created_at }}</td>
               </tr>
             </tbody>
