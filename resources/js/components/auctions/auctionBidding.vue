6631f51e95b5bffadf118651799e96f015da54eb
Maxim <maxim@MacBook-Pro-Maxim.local>
2020-04-03 19:43:39 +0600

    L

--- resources/js/components/auctions/auctionBidding.vue
+++ resources/js/components/auctions/auctionBidding.vue
@@ -68,7 +68,7 @@
                 {{ __('Auction volume') }}: {{ auction.volume }} | 
                 {{ __('Active volume') }}: {{ auction.free_volume }} | 
                 {{ __('Approved volume') }}: {{ auction.volume - auction.free_volume }} | 
-                {{ __('Approved volume') }}: {{ auction.undistributed }}
+                {{ __('Undistributed volume') }}: {{ auction.undistributed }}
               </th>
             </tr>
             <tr>
