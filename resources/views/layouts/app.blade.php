a20dd0d4a191e1b8d635abcfcab0f50c145775b2
Maxim <maxim@MacBook-Pro-Maxim.local>
2020-04-22 09:33:15 +0600

    M

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -200,7 +200,7 @@
     <!-- Footer -->
     @include ('footer')
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}?ver=3.37" defer></script>
+    <script src="{{ asset('js/app.js') }}?ver=3.40" defer></script>
     <script src="//code.jivosite.com/widget/SgKrZgYDyo" async></script>
 </body>
 
