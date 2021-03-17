890810952f9fff3ac7e5144c9fdc25ae4dc891e9
MaximBrewer <pimax1978@icloud.com>
2021-03-17 21:10:34 +0600

    fix

--- app/Http/Controllers/Api/V1/ResultsController.php
+++ app/Http/Controllers/Api/V1/ResultsController.php
@@ -46,10 +46,8 @@ class ResultsController extends Controller
     {
 
         return DB::table('bets')
-            ->leftJoin('auctions', 'bets.auction_id', '=', 'auctions.id')
-            ->leftJoin('contragents', 'bets.contragent_id', '=', 'contragents.id')
             ->leftJoin('defers', function ($join) {
-                $join->on('bets.contragent_id', '=', 'defers.creditor_id')->where('bets.distributor_id', '=', 'defers.supplier_id');
+                $join->on('bets.contragent_id', '=', 'defers.creditor_id')->on('bets.distributor_id', '=', 'defers.supplier_id');
             })
             ->select(['bets.*', 'defers.description', 'defers.orbits', 'defers.creditor_id'])
             ->where('bets.distributor_id', Auth::user()->contragents[0]->id)
