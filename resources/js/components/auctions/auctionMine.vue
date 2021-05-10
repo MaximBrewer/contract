20bba07ec98925b3d4173d3b579b5a683d6aca88
MaximBrewer <pimax1978@icloud.com>
2021-05-10 15:55:37 +0600

    fix

--- resources/js/components/auctions/auctionMine.vue
+++ resources/js/components/auctions/auctionMine.vue
@@ -177,7 +177,7 @@
                   </td>
                   <td>
                     <div class="text-nowrap">
-                      {{ bet.self }}
+                      {{ !!bet.self ? bet.self : "" }}
                     </div>
                   </td>
                 </tr>
