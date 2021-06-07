d99dac3b59b6e6b057f6d05df5f682240d02e051
MaximBrewer <pimax1978@icloud.com>
2021-06-07 13:13:45 +0600

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="http://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}?ver=3.52" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}?ver=3.53" rel="stylesheet">
     <script>
         window._translations = {!! cache('translations'); !!};
     </script>
@@ -217,7 +217,7 @@
     <!-- Footer -->
     @include ('footer')
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}?ver=3.52" defer></script>
+    <script src="{{ asset('js/app.js') }}?ver=3.53" defer></script>
     <script src="//code.jivosite.com/widget/SgKrZgYDyo" async></script>
 </body>
 
