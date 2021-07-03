5525384f9178ad6ce9f8eabc21a872273ef0bb82
MaximBrewer <pimax1978@icloud.com>
2021-07-03 13:59:58 +0600

    f

--- laravel-echo-server.js
+++ laravel-echo-server.js
@@ -1,6 +1,35 @@
 let echo = require('laravel-echo-server');
 
-echo.run(
-    // Конфиг из laravel-echo-server.json
-);
+echo.run({
+	"authHost": "https://localhost",
+	"authEndpoint": "/broadcasting/auth",
+	"clients": [],
+	"database": "redis",
+	"databaseConfig": {
+		"redis": {},
+		"sqlite": {
+			"databasePath": "/database/laravel-echo-server.sqlite"
+		}
+	},
+	"devMode": true,
+	"host": null,
+	"port": "6001",
+	"protocol": "https",
+	"socketio": {},
+	"secureOptions": 67108864,
+	"sslCertPath": "/etc/letsencrypt/live/project.cross-contract.ru/fullchain.pem",
+	"sslKeyPath": "/etc/letsencrypt/live/project.cross-contract.ru/privkey.pem",
+	"sslCertChainPath": "",
+	"sslPassphrase": "",
+	"subscribers": {
+		"http": true,
+		"redis": true
+	},
+	"apiOriginAllow": {
+		"allowCors": false,
+		"allowOrigin": "",
+		"allowMethods": "",
+		"allowHeaders": ""
+	}
+});
 
